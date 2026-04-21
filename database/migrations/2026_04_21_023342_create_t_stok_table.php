c229e61e7812f43a7eaff8fd0de278c8676730ee
Tsabita Keizya Vanya Putri <tsabitakeizya@gmail.com>
2026-04-21 09:45:58 +0700

    migration database

---
+++ database/migrations/2026_04_21_023342_create_t_stok_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+            $table->id('stok_id');
+
+            $table->unsignedBigInteger('barang_id');
+            $table->foreign('barang_id')
+                ->references('barang_id')
+                ->on('m_barang')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('supplier_id');
+            $table->foreign('supplier_id')
+                ->references('supplier_id')
+                ->on('m_supplier')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('user_id')
+                ->on('m_user')
+                ->onDelete('cascade');
+
+            $table->integer('stok_jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
